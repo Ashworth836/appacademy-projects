7f3787f0c9a52965f33c77a98992af0ae93e4578
ashworth836 <ashworthsakara13@gmail.com>
2023-02-09 15:17:15 +0200

    finished assignment

---
+++ error-types/validation-error.js
@@ -0,0 +1,21 @@
+// Your code here
+class ValidationError extends Error{
+  
+  constructor(message = 'Invalid input', ...params){
+    super(message, ...params);
+
+    if(Error.captureStackTrace){
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+    this.name = "ValidationError";
+  }
+}
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = ValidationError;
+} catch {
+  module.exports = null;
+}
