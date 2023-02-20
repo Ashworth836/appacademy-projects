6b96f06ee056d48e9363b8a0b1d5711d4147a75a
ashworth836 <ashworthsakara13@gmail.com>
2023-02-20 02:39:26 +0200

    finished assignment

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Import package
 
 // Your code here
+const jwt = require('jsonwebtoken');
 
 // Define variables - DO NOT MODIFY
 let token;
@@ -12,6 +13,11 @@ let payload;
 // 1. Sign (create) a JWT containing your email address
 
 // Your code here
+const token = jwt.sign( 
+    { email: "ashworthsakara@gmail.com" }, // payload object
+    process.env.SECRET_KEY,        // secret token from .env file
+    { expiresIn: '1h' }            // options (example: Token expires in 1 hour)
+);
 
 // See the JWT in the console
 console.log('JWT:', token);
@@ -19,6 +25,7 @@ console.log('JWT:', token);
 // 2. Decode a JWT Payload
 
 // Your code here
+const payload = jwt.decode(token);
 
 // See the decoded payload in the console
 console.log('Payload:', payload);
@@ -26,6 +33,7 @@ console.log('Payload:', payload);
 // 3. Verify a JWT
 
 // Your code here
+const payload = jwt.verify(token, process.env.NOT_THE_TOKEN);
 
 // See the verified payload in the console
 console.log('Verified Payload:', payload);
@@ -36,7 +44,12 @@ console.log('Verified Payload:', payload);
 //    Then "catch" the error and log it to the console.
 
 // Your code here
-
+try {
+    payload = jwt.verify(token, process.env.NOT_THE_TOKEN);
+    console.log('Verified Payload:', payload);
+} catch (err) {
+    console.error(err)
+}
 // (Optional) Bonus: Catch Error With Expired Token
 // First, set the token's expiration (above) to 1 second
 // Second, add a setTimeout longer than 1 second
@@ -44,3 +57,11 @@ console.log('Verified Payload:', payload);
 //    Then "catch" the error and log it to the console
 
 // Your code here
+setTimeout( () => {
+    try {
+        payload = jwt.verify(token, process.env.NOT_THE_TOKEN);
+        console.log('Verified Payload:', payload);
+    } catch (err) {
+        console.error(err)
+    }
+},2000)
