1c62dfa8b4d62085e469281b51a2c4b2b6e53d86
ashworth836 <ashworthsakara13@gmail.com>
2023-02-28 16:54:29 +0200

    Assignment Complete

---
+++ problems/15-subsets.js
@@ -0,0 +1,30 @@
+/***********************************************************************
+Write a function called `subsets` that will return all subsets of an array.
+
+Examples:
+
+subsets([]) // [[]]
+subsets([1]) // [[], [1]]
+subsets([1, 2]) // [[], [1], [2], [1, 2]]
+subsets([1, 2, 3]) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
+  1. Those that do not contain 3 (all of these are subsets of [1, 2]).
+  2. For every subset that does not contain 3, there is also a corresponding
+     subset that is the same, except it also does contain 3.
+***********************************************************************/
+
+// your code here
+function subsets(array){
+  if(array.length === 0) return [[]];
+
+  let element = array.slice(-1);
+  let prev = subsets(array.slice(0, -1));
+  return prev.concat(prev.map(arr => arr.concat(element)));
+}
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = subsets;
+} catch (e) {
+  module.exports = null;
+}
---
+++ problems/17-bonus-permutations.js
@@ -0,0 +1,40 @@
+/***********************************************************************
+Write a recursive method permutations(array) that calculates all the
+permutations of the given array. For an array of length n there are n! different
+permutations. So for an array with three elements we will have 3 * 2 * 1 = 6
+different permutations.
+
+Examples:
+
+permutations([1, 2]) // [[1, 2], [2, 1]]
+permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
+                        // [2, 1, 3], [2, 3, 1],
+                        // [3, 1, 2], [3, 2, 1]]
+***********************************************************************/
+
+// your code here
+function permutations(array){
+  let result = [];
+
+  if (array.length === 0) return [];
+  if (array.length === 1) return [array];
+
+  for (let i = 0; i < array.length; i++) {
+    const currentNum = array[i];
+    const remainingNums = array.slice(0, i).concat(array.slice(i +1));
+    const remainingNumsPermuted = permutations(remainingNums);
+
+    for (let j = 0; j < remainingNumsPermuted.length; j++) {
+      const permutedArray =  [currentNum].concat(remainingNumsPermuted[j]);
+      result.push(permutedArray);
+    }
+  }
+
+  return result;
+}
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = permutations;
+} catch (e) {
+  module.exports = null;
+}
