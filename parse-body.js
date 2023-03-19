907cf9f302df73424eff86fb93b8250cb4d73490
ashworth836 <ashworthsakara13@gmail.com>
2023-03-19 22:26:18 +0200

    Parse the Body of a Request Assignment Complete

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,31 @@
 function firstStep(input) {
   // Your code here
+  return input.split("&")
 }
 
 function secondStep(input) {
   // Your code here
+  return input.map(pair => pair.split("="));
 }
 
 function thirdStep(input) {
   // Your code here
+  return input.map(pair = pair.map(element = element.replaceAll("+", "")));
 }
 
 function fourthStep(input) {
   // Your code here
+  return input.map(pair = pair.map(element = decodeURICompnent(element)));
 }
 
 function fifthStep(input) {
   // Your code here
+  return Object.fromEntries(input);
 }
 
 function parseBody(str) {
   // Your code here
+  return fifthStep(fourthStep(thirdStep(secondStep(fifthStep(str)))));
 }
 
 /******************************************************************************/
