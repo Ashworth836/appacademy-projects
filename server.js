e5a53bae71247c421f477daae94723d8a60d1d55
ashworth836 <ashworthsakara13@gmail.com>
2023-03-20 16:06:07 +0200

    Creating Route Handlers Assignment Complete

--- server.js
+++ server.js
@@ -34,7 +34,58 @@ const server = http.createServer((req, res) => {
     // Do not edit above this line
 
     // define route handlers here
+    const urlSubStr = req.url.split('/');
+    let requestDogId = parseInt(req.url.split('/')[2] ? req.url.split('/')[2] : undefined);
+    
+    if(req.method === 'GET'){
+      
+      if(req.url === '/') {\
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Dog Club');
+      }
+      
+      if(urlSubStr.lrngth >= 2){
+        if(req.url === '/dogs'){
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          return res.end('Dogs index');
+        }
+      
+       if(urlSubStr[2] === 'new'){
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          return res.end('Dog create from page');
+        }
+  
+       if(res.url === `/dogs/${requestDogId}`){
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          return res.end(`Dog detail for godId: ${requestDogId}`);
+        }
+  
+        if(res.url === `/dogs/${requestDogId}/edit`){
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          return res.end(`Dog edit form page for dogId: ${requestDogId}`);
+        }
+     }  
+    }
 
+    if(req.method === 'POST'){
+
+      if(req.url === '/dog'){
+        res.statusCode = 302;
+        res.setHeader('Location', `/dogs/${requestDogId}`);
+        return res.end();
+      }
+
+      if(urlSubStr[2] === requestDogId){
+        res.statusCode = 302;
+        res.setHeader('Location', `/dogs/${requestDogId}`);
+        return res.end();
+      }
+    }
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
