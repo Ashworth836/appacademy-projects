edc926747af82e4fd5f7a17788845ee03a1fce54
ashworth836 <ashworthsakara13@gmail.com>
2023-03-21 14:39:46 +0200

    Catstagram Assignment Complete

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
 // Your code here
+window.addEventListener("DOMContentLoaded", event => {
+    const header = document.createElement("h1");
+    header.id = "kitten-header";
+    header.innerText = "Kitten Pic";
+
+    const kittenImg = document.createElement("img");
+    kittenImg.alt = "a photo of a cat";
+    kittenImg.src = "https://cdn2.thecatapi.com/images/7bp.jpg";
+
+    document.body.append(header);
+    document.body.appendChild(kittenImg);
+});
