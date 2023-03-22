d4520d24d83dfc218ac31b2f5b6f3fd19a7709a4
ashworth836 <ashworthsakara13@gmail.com>
2023-03-22 15:06:35 +0200

    Catsagram Phase 2: Add interactivity to the page Assignment Complete

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
-import { createMainContent } from './main.js';
+import { createMainContent, fetchNew, createVoteElements } from './main.js';
+import { createCommentsForm } from "./comments.js"
+import { saveAll, restoreData } from "./restore.js"
 
 const initializePage = () => {
     // Create container
     const container = document.createElement("section");
     container.className = "container";
+import { createMainContent } from './main.js';
     container.style.display = "flex";
     container.style.flexDirection = "column";
     container.style.alignItems = "center";
