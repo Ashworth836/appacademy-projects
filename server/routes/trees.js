c9e3e913b450a65703933ea2b39a69c017e731b7
ashworth836 <ashworthsakara13@gmail.com>
2023-04-11 15:40:07 +0200

    America's Largest Trees Assignment Complete

--- server/routes/trees.js
+++ server/routes/trees.js
@@ -6,20 +6,35 @@ const router = express.Router();
  * BASIC PHASE 2, Step A - Instantiate SQLite and database
  *   - Database file: "data_source" environment variable
  *   - Database permissions: read/write records in tables
- */
+*/
 // Your code here
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(DATA_SOURCE, sqlite3.OPEN_READWRITE);
 
 /**
  * BASIC PHASE 2, Step B - List of all trees in the database
- *
- * Protocol: GET
- * Path: /
- * Parameters: None
+*
+* Protocol: GET
+* Path: /
+* Parameters: None
  * Response: JSON array of objects
  *   - Object properties: height-ft, tree, id
  *   - Ordered by the height_ft from tallest to shortest
- */
+*/
 // Your code here
+router.get('/', (req, res) => {
+    sqlString = 'SELECT id, tree FROM trees ORDER BY height_ft DESC';
+    const params = [];
+
+    db.all(sqlString, params, (err, rows) => {
+        if(err){
+            next(err);
+        }else{
+            res.json(rows);
+        }
+    });
+
+});
 
 /**
  * BASIC PHASE 3 - Retrieve one tree with the matching id
@@ -29,8 +44,20 @@ const router = express.Router();
  * Parameter: id
  * Response: JSON Object
  *   - Properties: id, tree, location, height_ft, ground_circumference_ft
- */
+*/
 // Your code here
+router.get('/:id', (req, res) =>{
+    sqlString = 'SELECT * FROM trees WHERE id = ?';
+    params = [req.params.id];
+
+    db.all(sqlString, params, (err, rows) => {
+        if(err){
+            next(err);
+        } else {
+            res.json(rows);
+        }
+    });
+})
 
 /**
  * INTERMEDIATE PHASE 4 - INSERT tree row into the database
@@ -43,6 +70,25 @@ const router = express.Router();
  *   - Value: success
  */
 // Your code here
+router.post('/', (req, res) => {
+    const sql = `INSERT INTO trees (tree, location, height_ft, ground_circumference_ft)
+                VALUES (?, ?, ?, ?)`;
+    
+    const params = [
+        req.body.name,
+        req.body.location,
+        req.body.height,
+        req.body.size
+    ]
+    
+    db.all(sql, params, (err) => {
+        if(err){
+            next(err);
+        } else {
+            res.json({message: 'success'});
+        }
+    });
+});
 
 /**
  * INTERMEDIATE PHASE 5 - DELETE a tree row from the database
@@ -55,6 +101,19 @@ const router = express.Router();
  *   - Value: success
  */
 // Your code here
+router.delete('/trees/:id', (req, res, next) => {
+    sql = `DELETE FROM trees WHERE id = ?`;
+    params = [req.params.id];
+
+    do.all(sql, params (err) =>{
+        if(err){
+            next(err);
+        } else {
+            res.json(message: 'Successfully DELETED');
+        }
+    });
+
+});
 
 /**
  * INTERMEDIATE PHASE 6 - UPDATE a tree row in the database
@@ -67,6 +126,29 @@ const router = express.Router();
  *   - Value: success
  */
 // Your code here
+router.put('/trees/:id', (req, res) => {
+    const sql = `UPDATE trees 
+           SET tree = ?,
+            location = ?,
+            height_ft = ?,
+            ground_circumference_ft = ?
+           WHERE id = ?`;
 
+    const params = [
+        req.body.name,
+        req.body.location,
+        req.body.height,
+        req.body.size,
+        req.body.id
+    ];
+
+    db.run(sql, params, (err) => {
+        if(err){
+            next(err);
+        } else {
+            res.json({message: 'Success'});
+        }
+    });
+});
 // Export class - DO NOT MODIFY
 module.exports = router;
