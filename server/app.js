fb2f6d8a2d73763c1623c9d8294dcaed2d5a5bb9
ashworth836 <ashworthsakara13@gmail.com>
2023-04-13 16:20:07 +0200

    Inserting Data with Sequelize Assignment Complete

--- server/app.js
+++ server/app.js
@@ -17,6 +17,35 @@ app.get('/puppies', async (req, res, next) => {
     res.json(allPuppies);
 });
 
+// Step 1: Build a new puppy
+try{
+    const puppy = await Puppy.build({
+        name: 'Trudy',
+        age_yrs: 2,
+        weight_lbs: 38,
+        breed: 'Brittany Spaniel',
+        microchipped: false
+    });
+    await puppy.save();
+    console.log('Puppy successfully build and saved to the database');
+} catch (error){
+    console.error('Error building and saving puppy:', error);
+}
+
+// Step 2: Create a new puppy
+try {
+    const puppy = await Puppy.create({
+        name: 'Beans',
+        age_yrs: 1.6,
+        weight_lbs: 42,
+        breed: 'Bulldog',
+        microchipped: true
+    });
+    console.log('Puppy successfully build and saved to the database');
+    
+} catch (error) {
+    console.error('Error building and saving puppy:', error);
+}
 
 // STEP 3
 // Capture the name, age_yrs, breed, weight_lbs, and microchipped attributes
@@ -25,6 +54,19 @@ app.get('/puppies', async (req, res, next) => {
 // Respond to the request by sending a success message
 app.post('/puppies', async (req, res, next) => {
     // Your code here
+    try {
+        const puppy = await Puppy.create({
+            name: req.body.name,
+            age_yrs: req.body.age_yrs,
+            breed: req.body.breed,
+            weight_lbs: req.body.weight_lbs,
+            microchipped: req.body.microchipped
+        });
+        res.json({message: 'Puppy successfully build and saved to the database', data: puppy})
+    } catch (error) {
+        console.error('Error creating and saving puppy:', error);
+        res.status(500).json({ message: 'Error creating puppy.' });
+    }
 })
 
 
