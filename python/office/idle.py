b8c7d116b25323759f7b91a401fc427ff41d6a35
Ashworth836 <ashworthsakara13@gmail.com>
2023-05-15 16:33:09 +0200

    Python Import Practice Assignment Complete

--- python/office/idle.py
+++ python/office/idle.py
@@ -1,2 +1,24 @@
 
 # Your code here
+import random
+
+def chat():
+    coworkers = ["Jack", "Lenny", "Michelle", "Andrea"]
+    # chatee = coworkers[Math.floor(Math.random()*4)]
+    chatee = random.choice(coworkers)
+    print("Chatting with " + chatee + "...")
+    print("Done")
+
+
+def getWater():
+    print("Getting water...")
+    print("That was refreshing.")
+
+
+def useSocialMedia():
+    socialMedia = ["FaceBook", "Twitter", "YouTube", "Reddit"]
+    # choice = socialMedia[Math.floor(Math.random()*4)]
+    choice = random.choice(socialMedia)
+    print("Using " + choice + "...")
+    print("Done")
+
